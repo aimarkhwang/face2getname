8498c434dffe4de7659c3f3d37fe1c55901a8392
aimarkhwang <ai.markhwang@gmail.com>
2020-10-30 09:10:11 +0900

    랜덤 추가

--- nvapi.py
+++ nvapi.py
@@ -5,6 +5,7 @@ import requests
 from urllib.request import urlopen
 
 import json
+import random
 
 def face2getname(imgurl):
     client_id = ""
@@ -26,3 +27,9 @@ def face2getname(imgurl):
         print("Error Code:" + rescode)
 
 # get_name('https://image.bugsm.co.kr/artist/images/1000/800491/80049126.jpg')
+
+def rnd_name():
+    names = ['김대원','김경태','신윤한','이지훈','정회윤','조명진','김태환','김상희','강준필','조성활','김현정','송선종','신무철','배성환']
+    return random.choice(names)
+    
+print(rnd_name())
